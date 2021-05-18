0c0944d6ddf8d4c6a811653ebb167f746a2bc6ce
alex-lenk <alexlenk.ru@gmail.com>
2021-05-18 23:06:20 +0300

    learned a lesson 03_06_Класс для работы с DOM

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+import {Excel} from '@/components/excel/Excel'
+import {Header} from '@/components/header/Header'
+import {Toolbar} from '@/components/toolbar/Toolbar'
+import {Formula} from '@/components/formula/Formula'
+import {Table} from '@/components/table/Table'
 import './scss/index.scss'
 
-async function start() {
-  return await Promise.resolve('asinc workingqweqwe!')
-}
+const excel = new Excel('#app', {
+  components: [Header, Toolbar, Formula, Table]
+})
 
-start().then(console.log);
+excel.render()
